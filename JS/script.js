e524b27461c955b4375867c38a67f2337900b83a
loganshi <loganmendescontardi@gmail.com>
2023-07-19 13:03:16 -0300

    Atualização final do sistema

--- JS/script.js
+++ JS/script.js
@@ -11,7 +11,36 @@ class Produto{
             this.adicionar(produto)
         }
 
-        console.log(this.arrayProdutos);
+        this.listaTabela();
+        this.cancelar();
+    }
+
+    listaTabela(){
+        let tbody = document.getElementById('tbody');
+        tbody.innerText = '';
+        
+        for(let i = 0; i < this.arrayProdutos.length; i++){
+            let tr = tbody.insertRow();
+
+            let td_id = tr.insertCell();
+            let td_produto = tr.insertCell();
+            let td_valor = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_id.innerText = this.arrayProdutos[i].id;
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+            td_valor.innerText = this.arrayProdutos[i].preco;
+
+            let imgEdit = document.createElement('img');
+            imgEdit.src = 'IMG/escrever.png';
+
+            let imgDelete = document.createElement('img');
+            imgDelete.src = 'IMG/lata-de-lixo.png';
+            imgDelete.setAttribute("onclick","produto.deletar("+ this.arrayProdutos[i].id +")");
+
+            td_acoes.appendChild(imgEdit);
+            td_acoes.appendChild(imgDelete);
+        }
     }
 
     adicionar(produto){
@@ -49,7 +78,19 @@ class Produto{
     }
 
     cancelar(){
-        alert("Produto deletado");
+        document.getElementById('produto').value = '';
+        document.getElementById('preco').value = '';
+    }
+    deletar(id){
+
+        let tbody = document.getElementById('tbody');
+
+        for(let i = 0; 1 < this.arrayProdutos.length; i++){
+            if(this.arrayProdutos[i].id == id){
+                this.arrayProdutos.splice(i, 1);
+                tbody.deleteRow(i);
+            }
+        }
     }
 }
 var produto = new Produto();
